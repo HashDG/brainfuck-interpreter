6ef27605081f01e6f8271aabea292256d0ddd787
HashDG <hashdg2002@gmail.com>
2022-09-30 19:36:37 +0200

    Réduction de taille du fichier de source et de l'exécutable

--- main.c
+++ main.c
@@ -1,81 +1,30 @@
 #include <stdio.h>
-#include <string.h>
 #define MAX 30000
-
 int main(int argc, char* argv[]) {
-    if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
-        printf("Usage : ./brainfuck -[h] code.b");
-        return 0;
-    }
-
-    FILE *code = fopen(argv[argc - 1], "r");
-    if (code == NULL) {
-        printf("\nERROR: can't open brainfuck file");
-        return 1;
-    }
-
-    unsigned char board[MAX] = {0};
-    unsigned int stack[5] = {0};
-
-    unsigned char *ptr = board;
-    unsigned int *head = stack;
-
-    char instruction = (char) fgetc(code);
-
-    while (instruction != EOF) {
+	unsigned char board[MAX] = {0}, *ptr = board;
+    unsigned int stack[10] = {0}, *head = stack;
+    char instruction;
+    FILE *code;
+    if (argv[1] == "-h" || argv[1] == "--help" || argc == 1) { printf("Usage : ./brainfuck -[h] code.b"); return 0; }
+    if ( (code = fopen(argv[argc - 1], "r")) == NULL) return 1;
+    while ( (instruction = (char) fgetc(code)) != EOF && (&board[0] <= ptr && ptr <= &board[MAX])) {
         switch (instruction) {
-            case '+':
-                (*ptr)++;
-                break;
-            case '-':
-                (*ptr)--;
-                break;
-            case '>':
-                if (ptr != &board[MAX]) {
-                    ptr++;
-                    break;
-                }
-                return 2;
-            case '<':
-                if (ptr != &board[0]) {
-                    ptr--;
-                    break;
-                }
-                return 2;
-            case '.':
-                printf("%c", *ptr);
-                break;
-            case ',':;
-                char value;
-                scanf("%c", &value);
-                *ptr = value;
-                break;
+            case '+': (*ptr)++; break;
+            case '-': (*ptr)--; break;
+            case '>': ptr++; break;
+            case '<': ptr--; break; 
+            case '.': putchar(*ptr); break;
+            case ',': *ptr=getchar(); break;
             case '[':
                 if (*ptr) {
-                    if (*head) head++;
-                    *head = (int) ftell(code);
+                    *head = ( (*head) ? head++ : head ), *head = (int) ftell(code);
                 } else {
                     int loops = 0;
-                    while (instruction != ']' || loops) {
-                        if (instruction == '[') loops++;
-                        if (instruction == ']') loops--;
-                        instruction = (char) fgetc(code);
-                    }
+                    while ( (instruction = (char) fgetc(code)) != ']' || loops ) (instruction == '[') ? loops++ : ( (instruction == ']') ? loops-- : 0);
                 }
                 break;
-            case ']':
-                if (*ptr) {
-                    fseek(code, *head, SEEK_SET);
-                } else {
-                    head--;
-                }
-                break;
-            default:
-                if (instruction == '\n') break;
-                printf("\nERROR: Unknown Instruction: %c | %d", instruction, (int) instruction);
-                return 1;
+            case ']': (*ptr) ? (void) fseek(code, *head, SEEK_SET) : (void) head--; break;
         }
-        instruction = (char) fgetc(code);
     }
     return 0;
 }
